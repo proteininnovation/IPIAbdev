0aacf51ff33b4b5ec3f4387b458c88072a391f56
bmhoan <hoan.nguyen@proteininnovation.org>
2026-06-16 16:11:12 -0400

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 # models/__init__.py
 
-from .xgboost import XGBoostModel
+from .xgboost_bk import XGBoostModel
 from .randomforest import RandomForestModel
 from .cnn import CNNClassifier
 from .transformer_onehot import TransformerOneHotModel
-from .transformer_onehot_vh import TransformerOneHotModel_VH
+#from .transformer_onehot_vh import TransformerOneHotModel_VH
 from .transformer_lm_old import TransformerLMModel
 
 
@@ -13,6 +13,6 @@ available_models = {
     "rf": RandomForestModel,
     "cnn": CNNClassifier,
     "transformer_onehot": TransformerOneHotModel,   
-    "transformer_onehot_vh": TransformerOneHotModel_VH,
+    #"transformer_onehot_vh": TransformerOneHotModel_VH,
     "transformer_lm": TransformerLMModel
 }
